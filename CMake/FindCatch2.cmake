dae96cd47ed49d573ad2c04ae9fb6391e92ce8c3
JPMMaia <jpmmaia@gmail.com>
2018-09-02 14:27:53 +0300

    Starting migration from GoogleTest to Catch2.

---
+++ CMake/FindCatch2.cmake
@@ -0,0 +1,33 @@
+include (FetchContent)
+FetchContent_Declare (
+    Catch2
+    GIT_REPOSITORY "https://github.com/catchorg/Catch2.git"
+    GIT_TAG "v2.3.0"
+)
+
+FetchContent_GetProperties (Catch2)
+if (NOT catch2_POPULATED)
+    
+    FetchContent_Populate (Catch2)
+
+    find_path (catch2_INCLUDE_DIR "catch2/catch.hpp" HINTS "${catch2_SOURCE_DIR}/single_include" NO_DEFAULT_PATH)
+
+    include (FindPackageHandleStandardArgs)
+    find_package_handle_standard_args (Catch2 REQUIRED_VARS catch2_INCLUDE_DIR)
+
+    if (Catch2_FOUND AND NOT TARGET Catch2::Catch2)
+        
+        add_subdirectory ("${catch2_SOURCE_DIR}" "${catch2_BINARY_DIR}")
+
+        list (APPEND CMAKE_MODULE_PATH "${catch2_SOURCE_DIR}/contrib")
+
+    endif()
+
+else ()
+
+    find_path (catch2_INCLUDE_DIR "catch2/catch.hpp" HINTS "${catch2_SOURCE_DIR}/single_include" NO_DEFAULT_PATH)
+
+    include (FindPackageHandleStandardArgs)
+    find_package_handle_standard_args (Catch2 REQUIRED_VARS catch2_INCLUDE_DIR)
+
+endif ()
