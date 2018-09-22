33b05ff534caa5d201caf734071a6d632ec4c6f7
JPMMaia <jpmmaia@gmail.com>
2018-09-22 11:35:56 +0300

    Update Catch2 version.

--- CMake/FindCatch2.cmake
+++ CMake/FindCatch2.cmake
@@ -2,7 +2,7 @@ include (FetchContent)
 FetchContent_Declare (
     Catch2
     GIT_REPOSITORY "https://github.com/catchorg/Catch2.git"
-    GIT_TAG "v2.3.0"
+    GIT_TAG "v2.4.0"
 )
 
 FetchContent_GetProperties (Catch2)
